b728d87600dd4c3b285db808598ee7721014737b
Hack0333 <absharma0333@gmail.com>
2024-10-10 14:23:05 +0530

    responsive

--- miniProject/app.js
+++ miniProject/app.js
@@ -11,10 +11,10 @@ const { isLoggedIn } = require("./middleware/auth.js");
 const mongoose = require("mongoose");
 
 
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 
 const connectDb = async () => {
