28380940137daf0290967f3077ad15c2844fe62d
Hack0333 <absharma0333@gmail.com>
2024-10-10 12:30:11 +0530

    minni project final

--- miniProject/app.js
+++ miniProject/app.js
@@ -6,7 +6,7 @@ const userModel = require("./models/user.js");
 const Post = require("./models/post.js");
 const cookieParser = require("cookie-parser");
 const app = express();
-const {isLoggedIn} = require("./middleware/auth.js");
+const { isLoggedIn } = require("./middleware/auth.js");
 const { log } = require("console");
 
 app.use(express.json());
@@ -39,8 +39,7 @@ app.post("/register", async (req, res) => {
 
           const token = jwt.sign(
             { userid: createdUser._id, email: createdUser.email },
-            "Deepak@123",
-            { expiresIn: "1h" }
+            "Deepak@123"
           );
 
           res.cookie("token", token);
@@ -59,17 +58,19 @@ app.post("/register", async (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
-app.post("/login", async(req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await userModel.findOne({ email });
     if (!user) {
       return res.status(404).send({ message: "Something went wrong" });
-      
     } else {
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
-        const token = jwt.sign({userid: user._id, email: user.email}, "Deepak@123");
+        const token = jwt.sign(
+          { userid: user._id, email: user.email },
+          "Deepak@123"
+        );
 
         res.cookie("token", token);
 
@@ -84,35 +85,35 @@ app.post("/login", async(req, res) => {
     res.status(500).json({ message: "Error Login user", error: error.message });
   }
 });
-app.get('/profile',isLoggedIn,async(req,res)=>{
+app.get("/profile", isLoggedIn, async (req, res) => {
   // const user = await userModel.findOne({_id:req.user._id}).populate('posts');
-  const allPosts = await Post.find({}).populate('author');
-  res.render('profile',{user : req.user , allPosts});
-})
-app.post('/post',isLoggedIn,async(req,res)=>{
-  const {title , content} = req.body;
-  const user = await userModel.findOne({_id:req.user._id});
+  const allPosts = await Post.find({}).populate("author");
+  res.render("profile", { user: req.user, allPosts });
+});
+app.post("/post", isLoggedIn, async (req, res) => {
+  const { title, content } = req.body;
+  const user = await userModel.findOne({ _id: req.user._id });
   const post = await Post.create({
     title,
     content,
-    author : user._id
-  })
+    author: user._id,
+  });
   await post.save();
   user.posts.push(post._id);
   await user.save();
-  res.redirect('/profile');
-})
-app.get('/logout',(req,res)=>{
+  res.redirect("/profile");
+});
+app.get("/logout", (req, res) => {
   res.clearCookie("token");
-  res.redirect('/login');
-})
+  res.redirect("/login");
+});
 
-app.get('/like/:id', isLoggedIn, async (req, res) => {
+app.get("/like/:id", isLoggedIn, async (req, res) => {
   try {
-    const post = await Post.findOne({_id : req.params.id});
+    const post = await Post.findOne({ _id: req.params.id });
     // console.log(post);
     if (!post) {
-      return res.status(404).send('Post not found');
+      return res.status(404).send("Post not found");
     }
 
     const likeIndex = post.likes.indexOf(req.user._id);
@@ -123,54 +124,51 @@ app.get('/like/:id', isLoggedIn, async (req, res) => {
     }
 
     await post.save();
-    res.redirect('/profile');
+    res.redirect("/profile");
   } catch (error) {
-    console.error('Error liking/unliking post:', error);
-    res.status(500).send('An error occurred');
+    console.error("Error liking/unliking post:", error);
+    res.status(500).send("An error occurred");
   }
-})
-
-app.get('/delete/:id', isLoggedIn, async (req, res) => {
-    try {
-        const postId = req.params.id;
-        const post = await Post.findById(postId);
-
-        if (!post) {
-            return res.status(404).send('Post not found');
-        }
-
-        // if (!post.author.equals(req.user._id)) {
-        //     return res.status(403).send('You are not authorized to delete this post');
-        // }
-
-        await Post.findByIdAndDelete(postId);
-
-        await userModel.updateOne(
-            { _id: req.user._id },
-            { $pull: { posts: postId } }
-        );
-
-        res.redirect('/profile');
-    } catch (error) {
-        console.error('Error deleting post:', error);
-        res.status(500).send('An error occurred while deleting the post');
-    }
 });
 
-app.get('/edit/:id',isLoggedIn,async(req,res)=>{
+app.get("/delete/:id", isLoggedIn, async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).send("Post not found");
+    }
+
+    // if (!post.author.equals(req.user._id)) {
+    //     return res.status(403).send('You are not authorized to delete this post');
+    // }
+
+    await Post.findByIdAndDelete(postId);
+
+    await userModel.updateOne(
+      { _id: req.user._id },
+      { $pull: { posts: postId } }
+    );
+
+    res.redirect("/profile");
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    res.status(500).send("An error occurred while deleting the post");
+  }
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
   const post = await Post.findById(req.params.id);
-  res.render('edit',{post});
-})
+  res.render("edit", { post });
+});
 
-app.post('/edit/:id',isLoggedIn,async(req,res)=>{
-  const {title,content} = req.body;
+app.post("/edit/:id", isLoggedIn, async (req, res) => {
+  const { title, content } = req.body;
   const postiId = req.params.id;
-  const post = await Post.findByIdAndUpdate(postiId,
-    {title , content}
-  )
+  const post = await Post.findByIdAndUpdate(postiId, { title, content });
   await post.save();
-  res.redirect('/profile');
-})
-
+  res.redirect("/profile");
+});
 
 app.listen(3000);
