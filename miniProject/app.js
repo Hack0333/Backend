b995e1002a2dc1a7e90dffc091dc377d056c8b8d
Hack0333 <absharma0333@gmail.com>
2024-10-10 15:28:01 +0530

    add favicon

--- miniProject/app.js
+++ miniProject/app.js
@@ -15,7 +15,9 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("public"));
+
 
 const connectDb = async () => {
   try {
